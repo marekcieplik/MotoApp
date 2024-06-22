3ef78ed1f7b3663c158e8238ad8764bca09bcd02
Marek Cieplik <marek.cieplik@gmail.com>
2024-06-22 09:19:38 +0200

    Basic Stack Implementation https://edu.gotoit.pl/index.php/kurs-c-od-podstaw-poziom-sredniozaawansowany/wprowadzenie/implementacja-basic-stack/

---
+++ MotoApp/MotoApp/BasicStack.cs
@@ -0,0 +1,20 @@
+﻿namespace MotoApp
+{
+    internal class BasicStack
+    {
+        //table of etams in stock
+        private readonly double[] _items;
+        //current index of actual item
+        private int _index = -1;
+
+        //constructor in new form
+        public BasicStack() => _items = new double[10];
+
+        //stack items counter
+        public int Counter => _index + 1;
+
+        //methods in stack
+        public void Push(double value) => _items[++_index] = value;
+        public double Pop() => _items[_index--];
+    }
+}
