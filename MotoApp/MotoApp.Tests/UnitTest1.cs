10af2aea13952e6c3480d107e2f5d8ba40a9f624
Marek Cieplik <marek.cieplik@gmail.com>
2024-06-27 07:10:26 +0200

    test; sum items  https://edu.gotoit.pl/index.php/kurs-c-od-podstaw-poziom-sredniozaawansowany/wprowadzenie/stos-dla-roznych-typow-wstep-do-generykow/

--- MotoApp/MotoApp.Tests/UnitTest1.cs
+++ MotoApp/MotoApp.Tests/UnitTest1.cs
@@ -13,5 +13,23 @@ namespace MotoApp.Tests
             Assert.AreEqual(1, testos.Count);
             Assert.That(testos.Count, Is.EqualTo(1));
         }
+
+        [Test]
+        public void When_PushItems_Pop_CountSum()
+        {
+            var testos = new BasicStack();
+            testos.Push(4.5);
+            testos.Push(43);
+            testos.Push(333.6);
+            // act
+            double sum = 0.0;
+            while (testos.Count > 0)
+            {
+                sum += testos.Pop();
+            }
+            // assert
+            Assert.That(sum, Is.EqualTo(381.1));
+
+        }
     }
 }
