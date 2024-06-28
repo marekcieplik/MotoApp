4243b7c0ae5eedf826d523ac1ec3e3600df557fd
Marek Cieplik <marek.cieplik@gmail.com>
2024-06-28 18:31:38 +0200

    Employee, EmployeeRepository

---
+++ MotoApp/MotoApp/Entites/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace MotoApp.Entites
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public Employee(string firstname)
+        {
+            FirstName = firstname;
+        }
+
+        public override string ToString() => $"Employee Id: {Id}, FirstName: {FirstName}";
+    }
+}
--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,24 +1,8 @@
-﻿using MotoApp;
-var stac = new BasicStack<double>();
-stac.Push(4.5);
-stac.Push(43);
-stac.Push(333.6);
+﻿using MotoApp.Repisitories;
+using MotoApp.Entites;
 
-var stacstring = new BasicStack<string>();
-stacstring.Push("hallo");
-stacstring.Push("Company");
-
-double sum = 0.0;
-while (stac.Count > 0)
-{
-    double item = stac.Pop();
-    Console.WriteLine($"item: {item}");
-    sum += item;
-}
-Console.WriteLine(sum);
-
-Console.WriteLine("stac strin");
-while (stacstring.Count > 0)
-{
-    Console.WriteLine(stacstring.Pop());
-}
+var employeeRepository  = new EmployeeRepository();
+employeeRepository.Add(new Employee("Adam"));
+employeeRepository.Add(new Employee("Piotr"));
+employeeRepository.Add(new Employee("Zuzia"));
+employeeRepository.Save();
